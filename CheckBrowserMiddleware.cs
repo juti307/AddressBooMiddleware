23768f7b96ae8757a35829c2a3e7d39b00354de7
Julita <77607@student.pb.edu.pl>
2021-04-26 23:47:44 +0200

    Dodaj pliki projektów.

---
+++ CheckBrowserMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wangkanai.Detection.Models;
+using Wangkanai.Detection.Services;
+
+namespace AddressBookPS02
+{
+    public class CheckBrowserMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public CheckBrowserMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        
+        public Task Invoke(HttpContext context, IDetectionService detection)
+        {
+            if (detection.Browser.Name == Browser.InternetExplorer || detection.Browser.Name == Browser.Edge)
+            {
+                context.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+                return _next(context);
+            }
+            else
+            {
+                
+                return _next(context);
+            }
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using AddressBookPS02.Model;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+
+namespace AddressBookPS02.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty] //przypinamy by był to element przesyłany w postcie, z domyślnymi ustawieniami
+        public Address Address { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+        public IActionResult OnPost() //metoda on post
+        {
+            if (ModelState.IsValid)
+            {
+                HttpContext.Session.SetString("SessionAddress",
+                JsonConvert.SerializeObject(Address));
+                return RedirectToPage("./Address");
+            }
+            return Page();
+            //return RedirectToPage("./Privacy"); // przejdź do strony Privaacy
+        }
+    }
+}
